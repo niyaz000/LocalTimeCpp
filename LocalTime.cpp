b1ad8048dc367fd83c505851560a13990a7c07e4
Niyaz K <niyazk2991@gmail.com>
2020-09-19 13:29:10 +0000

    Minor changes.

--- LocalTime.cpp
+++ LocalTime.cpp
@@ -6,16 +6,54 @@ void validateTime(int64_t hour, int64_t minute, int64_t second) {
   if (hour > 23 || hour < 0) {
     throw std::out_of_range("hour should be between (0, 23) inclusive");
   }
-  if(minute > 59 || minute < 0) {
+  if (minute > 59 || minute < 0) {
     throw std::out_of_range("minute should be between (0, 59) inclusive");
   }
-  if(second > 59 || second < 0) {
+  if (second > 59 || second < 0) {
     throw std::out_of_range("second should be between (0, 59) inclusive");
   }
 }
 
-LocalTime::LocalTime(int64_t hour, int64_t minute, int64_t second): hour(hour), minute(minute), second(second), nano_second(0) {
+int64_t to_hour(int64_t epoch_time) {
+  return (epoch_time/(60 * 60)) % 24;
+}
+
+int64_t to_minute(int64_t epoch_time) {
+  return (epoch_time/60) % 60;
+}
+
+int64_t to_second(int64_t epoch_time) {
+  return (epoch_time) % 60;
+}
+
+LocalTime::LocalTime(int64_t hour, int64_t minute, int64_t second):
+  hour(hour),
+  minute(minute),
+  second(second),
+  nano_second(0) {
   validateTime(hour, minute, second);
+}
+
+LocalTime::LocalTime(int64_t hour,
+                     int64_t minute,
+                     int64_t second,
+                     int64_t nano_second) :
+  hour(hour),
+  minute(minute),
+  second(second),
+  nano_second(nano_second) {
+
+}
+
+LocalTime::LocalTime(int64_t epoch_time, int64_t nano_second):
+  hour(to_hour(epoch_time)),
+
+  minute(to_minute(epoch_time)),
+
+  second(to_second(epoch_time)),
+
+  nano_second(nano_second) {
+
 }
 
 int64_t LocalTime::get_hour() const {
@@ -35,21 +73,33 @@ int64_t LocalTime::to_nanosecond() const {
 }
 
 LocalTime LocalTime::with(int64_t hour) {
-  return LocalTime(hour, 0, 0);
+  return {hour, 0, 0};
 }
 
 LocalTime LocalTime::with(int64_t hour, int64_t minute) {
-  return LocalTime(hour, minute, 0);
+  return {hour, minute, 0};
 }
 
 LocalTime LocalTime::with(int64_t hour, int64_t minute, int64_t second) {
-  return LocalTime(hour, minute, second);
+  return {hour, minute, second};
 }
 
 LocalTime LocalTime::plus_hour(int64_t hour) {
-  return LocalTime(this->hour + hour, this->minute, this->second);
+  return {this->hour + hour, this->minute, this->second, this->nano_second};
 }
 
 LocalTime LocalTime::minus_hour(int64_t hour) {
-  return LocalTime(this->hour - hour, this->minute, this->second);
+  return {(this->hour - hour) % 24, this->minute, this->second, this->nano_second};
+}
+
+LocalTime LocalTime::withHour(int64_t hour) {
+  return {hour, this->minute, this->second, this->nano_second};
+}
+
+LocalTime LocalTime::withMinute(int64_t minute) {
+  return {this->hour, minute, this->second, this->nano_second};
+}
+
+LocalTime LocalTime::withSecond(int64_t second) {
+  return {this->hour, this->minute, second, this->nano_second};
 }
